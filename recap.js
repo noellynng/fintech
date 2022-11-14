41c8437f65e8b0fbbfc3065805293944b2b21a37
Noellyn <noellynng@hotmail.com>
2022-11-14 12:28:51 +0800

    test

---
+++ recap.js
@@ -0,0 +1,16 @@
+records = [
+    {
+        name: "Dixant",
+        email: "dixant@gmail.com",
+        wallet: 14,
+    }
+]
+console.log(records[1].name);
+
+let age = 21;
+
+if (age<18) {
+    console.log("Not eligible to vote yet.");
+} else {
+    console.log("Please cast your vote.");
+}
